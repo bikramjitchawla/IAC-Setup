74c0eb1748f40083712933bc234a306439fbf2f0
bikramjitchawla <bkjit99@gmail.com>
2025-11-08 22:40:53 +0100

    eks yaml updated

--- terraform-ec2-lab/main.tf
+++ terraform-ec2-lab/main.tf
@@ -1,44 +1,69 @@
 resource "aws_vpc" "lab_vpc" {
-    cidr_block =  "10.0.0.0/16"
-    tags = {
-        Name = "terraform-lab-vpc"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-lab-vpc"
+  }
 }
 
-resource "aws_subnet" "lab_subnet" {
-    vpc_id = aws_vpc.lab_vpc.id
-    cidr_block        = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "eu-north-1a"
-    tags = {
-        Name = "terraform-lab-subnet"
-    }
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.lab_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "lab-public-1"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.lab_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-north-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "lab-public-2"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
 resource "aws_internet_gateway" "lab_igw" {
-    vpc_id = aws_vpc.lab_vpc.id
-    tags = {
-        Name= "terraform-lab-igw"
-    }
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "terraform-lab-igw"
+  }
 }
 
-resource "aws_route_table" "lab_rt"{
-    vpc_id = aws_vpc.lab_vpc.id
-    route {
-        cidr_block = "0.0.0.0/16"
-        gateway_id = aws_internet_gateway.lab_igw.id
-    }
+
+resource "aws_route_table" "lab_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"               
+    gateway_id = aws_internet_gateway.lab_igw.id
+  }
+
+  tags = {
+    Name = "terraform-lab-rt"
+  }
 }
 
-# associate route table with subnet 
-
-resource "aws_route_table_association" "lab_rta"{
-    subnet_id = aws_subnet.lab_subnet.id
-    route_table_id = aws_route_table.lab_rt.id
-
+resource "aws_route_table_association" "rta_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.lab_rt.id
 }
 
-# security group 
+resource "aws_route_table_association" "rta_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.lab_rt.id
+}
+
 resource "aws_security_group" "lab_sg" {
   name        = "terraform-lab-sg"
   description = "Allow SSH and HTTP"
@@ -72,14 +97,14 @@ resource "aws_security_group" "lab_sg" {
   }
 }
 
-resource "aws_instance" "lab_instance"{
-    ami = "ami-0d4d7e98021b6b9a6"
-    instance_type = var.instance_type
-    subnet_id = aws_subnet.lab_subnet.id
-    vpc_security_group_ids = [aws_security_group.lab_sg.id]
-    key_name = var.key_name
+resource "aws_instance" "lab_instance" {
+  ami                    = "ami-0d4d7e98021b6b9a6"
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.lab_sg.id]
+  key_name               = var.key_name
 
-    tags = {
-        Name = "terraform-lab-instance"
-    }
+  tags = {
+    Name = "terraform-lab-instance"
+  }
 }
